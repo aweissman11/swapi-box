8abcfa983fb55b2cbae377806ef219b1a8109da1
Aaron Weissman <weissman.aaron@gmail.com>
2018-10-09 18:50:04 -0600

    nav btns work

--- src/App/App.js
+++ src/App/App.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
 
 import ScrollSection from '../scroll-section/scroll-section';
+import CardsContainer from '../cards-container/cards-container';
 import Navigation from '../navigation/navigation';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      currentPage: 'scroll'
+    }
+  }
+
+  changePage = (page) => this.setState({currentPage: page})
+
   render() {
+    const { currentPage } = this.state
+
     return (
       <div className="App">
-        <ScrollSection />
-        <Navigation />
-        <h1>STAR</h1>
-        <h2>The Return of the Semi</h2>
-        <h1>WARS</h1>
+        <Navigation
+          changePage={this.changePage}
+        />
+        {
+          (currentPage === 'scroll') ? 
+          <ScrollSection /> : <CardsContainer 
+                                currentPage={currentPage}
+                                />
+        }
       </div>
     );
   }
