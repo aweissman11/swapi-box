f1f888dd1c26d0b4ec0cff98d085fa3f608da616
Aaron Weissman <weissman.aaron@gmail.com>
2018-10-10 13:32:21 -0600

    random movie text now displays

--- src/scroll-section/scroll-section.js
+++ src/scroll-section/scroll-section.js
@@ -2,23 +2,46 @@ import React, { Component } from 'react';
 import Crawl from 'react-star-wars-crawl';
 import 'react-star-wars-crawl/lib/index.css';
 
-
+// import * as SWRepository from '../helper';
 import './scroll-section.css';
 
-const MyCrawlComponent = () => (
+
+
+
+
+
+
+const MyCrawlComponent = (props) => (
     <Crawl
-      title="Episode V"
-      subTitle="The Event Loop Strikes Back"
-      text="It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire. During the battle, Rebel spies managed to steal secret plans to the Empire’s ultimate weapon, the DEATH STAR, an armored space station with enough power to destroy an entire planet. Pursued by the Empire’s sinister agents, Princess Leia races home aboard her starship, custodian of the stolen plans that can save her people and restore freedom to the galaxy…"
+      title='STAR WARS'
+      subTitle={props.movie.title}
+      text={
+        <div>
+          <p>{props.movie.opening}</p>
+          <br />
+          <p>Released: {props.movie.date}</p>
+        </div>
+      }
     >
     </Crawl>
 )
 
+
+
+
+
+
+
 class ScrollSection extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+
   render() {
     return (
       <div className="ScrollSection">
-        <MyCrawlComponent />
+        <MyCrawlComponent movie={this.props.movie}/>
       </div>
     );
   }
