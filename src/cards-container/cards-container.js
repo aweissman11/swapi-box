ff91ea387fb23867d2379cd8d6626743bf0abc97
Aaron Weissman <weissman.aaron@gmail.com>
2018-10-11 12:03:28 -0600

    implement fetch all into cards container and set loading screen up to be built

--- src/cards-container/cards-container.js
+++ src/cards-container/cards-container.js
@@ -1,111 +1,56 @@
 import React, { Component } from 'react';
 
+import Card from '../card/card';
 import './cards-container.css';
 
 class CardsContainer extends Component {
-  // constructor(props) {
-  //   super(props)
-  // }
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      pageRepo: []
+    }
+  }
+
+  async componentDidMount() {
+    const { fetchMethods, currentPage } = this.props
+    let pageRepo
+
+    switch(currentPage) {
+      case('people') :
+        pageRepo = await fetchMethods.getItemList(currentPage)
+
+    }
+
+    this.setState({ pageRepo })
+  }
+
+  listCards = () => {
+    return this.state.pageRepo.map( stats => <Card stats={stats} />)
+  }
 
   render() {
-    return(
-      <div className='cards-container'>
-        <div className='container'>
-          <h1>STAR WARS</h1>
-          <h2>The Return of the Semi</h2>
-          <h2>{this.props.currentPage}</h2>
-          <div className='cards'>
-            <div className='card'>
-              <h4>title</h4>
-              <p>cards</p>
-              <p>cards</p>
-              <p>cards</p>
-              <p>cards</p>
-              <p>cards</p>
-            </div>
-            <div className='card'>
-              <h4>title</h4>
-              <p>cards</p>
-              <p>cards</p>
-              <p>cards</p>
-              <p>cards</p>
-              <p>cards</p>
-            </div>
-            <div className='card'>
-              <h4>title</h4>
-              <p>cards</p>
-              <p>cards</p>
-              <p>cards</p>
-              <p>cards</p>
-              <p>cards</p>
-            </div>
-            <div className='card'>
-              <h4>title</h4>
-              <p>cards</p>
-              <p>cards</p>
-              <p>cards</p>
-              <p>cards</p>
-              <p>cards</p>
-            </div>
-            <div className='card'>
-              <h4>title</h4>
-              <p>cards</p>
-              <p>cards</p>
-              <p>cards</p>
-              <p>cards</p>
-              <p>cards</p>
-            </div>
-            <div className='card'>
-              <h4>title</h4>
-              <p>cards</p>
-              <p>cards</p>
-              <p>cards</p>
-              <p>cards</p>
-              <p>cards</p>
-            </div>
-            <div className='card'>
-              <h4>title</h4>
-              <p>cards</p>
-              <p>cards</p>
-              <p>cards</p>
-              <p>cards</p>
-              <p>cards</p>
-            </div>
-            <div className='card'>
-              <h4>title</h4>
-              <p>cards</p>
-              <p>cards</p>
-              <p>cards</p>
-              <p>cards</p>
-              <p>cards</p>
-            </div>
-            <div className='card'>
-              <h4>title</h4>
-              <p>cards</p>
-              <p>cards</p>
-              <p>cards</p>
-              <p>cards</p>
-              <p>cards</p>
-            </div>
-            <div className='card'>
-              <h4>title</h4>
-              <p>cards</p>
-              <p>cards</p>
-              <p>cards</p>
-              <p>cards</p>
-              <p>cards</p>
-            </div>
-            <div className='card'>
-              <h4>title</h4>
-              <p>cards</p>
-              <p>cards</p>
-              <p>cards</p>
-              <p>cards</p>
-              <p>cards</p>
+    const { pageRepo } = this.state;
+    return (
+      <div>
+    {
+      (pageRepo.length < 1) ?
+          <div>
+            <h1> No Repo </h1>
+          </div> : 
+          <div className='cards-container'>
+            <div className='container'>
+              <h1>STAR WARS</h1>
+              <h2>The Return of the Semi</h2>
+              <h2>{this.props.currentPage}</h2>
+              <div className='cards'>
+                { this.listCards() }
+              </div>
             </div>
           </div>
-        </div>
-      </div>
+      
+    }
+    </div>
     )
   }
 }
