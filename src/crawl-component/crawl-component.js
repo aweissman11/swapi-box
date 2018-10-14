5eb84dc32c8578f37d76a5f56d5148f13007cb13
Aaron Weissman <weissman.aaron@gmail.com>
2018-10-14 16:57:58 -0600

    craw text is now spans instead of p tags to avoid an error

--- src/crawl-component/crawl-component.js
+++ src/crawl-component/crawl-component.js
@@ -3,18 +3,18 @@ import Crawl from 'react-star-wars-crawl';
 import 'react-star-wars-crawl/lib/index.css';
 
 const MyCrawlComponent = (props) => (
-    <Crawl
-      title='STAR WARS'
-      subTitle={props.movie.title}
-      text={
-        <div>
-          <p>{props.movie.opening}</p>
-          <br />
-          <p>Released: {props.movie.date}</p>
-        </div>
-      }
-    >
-    </Crawl>
-)
+  <Crawl
+    title='STAR WARS'
+    subTitle={props.movie.title}
+    text={
+      <span>
+        <span>{props.movie.opening}</span>
+        <br />
+        <span>Released: {props.movie.date}</span>
+      </span>
+    }
+  >
+  </Crawl>
+);
 
-export default MyCrawlComponent
+export default MyCrawlComponent;
