4e0e12a8e89d3c545f43222c63df065b8b783746
Sara Ebbers <johndoe@example.com>
2018-10-17 08:00:23 -0600

    Add constructor LocalStorage to setupTests

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,20 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() })
-  
+
+class LocalStorage{
+  constructor() {
+    this.store= {}
+  }
+  setItem = (key, JSONValue)=> {
+    this.store[key]=JSONValue
+  }
+  getItem = (key) => {
+    return this.store[key] || null
+  }
+  clear = () => {
+    this.store = {}
+  }
+}
+
+global.localStorage = new LocalStorage();
