dac59a207e12b5794baa706f15526e0633b21977
Aaron Weissman <weissman.aaron@gmail.com>
2018-10-17 07:39:04 -0600

    add random number test

--- src/helper.test.js
+++ src/helper.test.js
@@ -57,112 +57,115 @@ describe('helper.js', () => {
     // })
       
   });
-    
-  describe('cleanMovieText', () => {
-    let mockuncleanMovies;
-    // let mockrandomNumber
+  
+  describe('randomNumber', () => {
       
-    beforeEach(() => {
-      mockuncleanMovies = [
-        { title: 'StarTrek', date: 'December 31 1999', opening: 'Space: the final frontier.  These are the voyages of the starship Enterprise.  Its five-year mission: to explore strange new worlds. To seek out new life and new civilizations.  To boldly go where no man has gone before!', somethingElse: 'info that will later be removed from this object'},
+    it('should return a randomNumber between 0 and 6', () => {
+      //set-up
+      let rndm = randomNumber();
+      //execution
+      // let rndm = Math.floor(Math.random() * Math.floor(6));
+      //expectation
+      expect(rndm).toBeLessThan(6.1);
+      expect(rndm).toBeGreaterThan(-.1);
+    });
+
+    describe('getMovieText', () => {
+      let mockuncleanMovies;
+      let mockmovieObject;
+      // let mockrandomNumber
+      
+      beforeEach(() => {
+        window.fetch = jest.fn().mockImplementation(() => Promise.resolve({ok: true, json: () => Promise.resolve(mockmovieObject)}));
+        mockmovieObject = { results: [
+          { title: 'StarTrek', date: 'December 31 1999', opening: 'Space: the final frontier.  These are the voyages of the starship Enterprise.  Its five-year mission: to explore strange new worlds. To seek out new life and new civilizations.  To boldly go where no man has gone before!', somethingElse: 'info that will later be removed from this object'},
+          { title: 'A Tale of Two Cities', date: 'January 1 2000', opening: 'It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we', somethingElse: 'info that will be taken out of this object'}
+        ] };
+      });
+
+      it('should return just one movie', async () => {
+        let movie = await getMovieText();
+        expect(Object.keys(movie)).toEqual(['title', 'date', 'opening']);
+      });
+    });
+  
+    describe('cleanMovieText', () => {
+      let mockuncleanMovies;
+      
+      beforeEach(() => {
+        mockuncleanMovies = [
+          { title: 'StarTrek', date: 'December 31 1999', opening: 'Space: the final frontier.  These are the voyages of the starship Enterprise.  Its five-year mission: to explore strange new worlds. To seek out new life and new civilizations.  To boldly go where no man has gone before!', somethingElse: 'info that will later be removed from this object'},
           
-        { title: 'A Tale of Two Cities', date: 'January 1 2000', opening: 'It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we', somethingElse: 'info that will be taken out of this object'}
-      ];
-    });
-    it('should return the same number of movies', () => {
+          { title: 'A Tale of Two Cities', date: 'January 1 2000', opening: 'It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we', somethingElse: 'info that will be taken out of this object'}
+        ];
+      });
+      it('should return the same number of movies', () => {
       //set-up
-      const expected = mockuncleanMovies.length;
-      //execution
+        const expected = mockuncleanMovies.length;
+        //execution
         
-      let cleanMovies = cleanMovieText(mockuncleanMovies);
+        let cleanMovies = cleanMovieText(mockuncleanMovies);
+        //expectation
+        expect(cleanMovies.length).toEqual(expected);
+      });
+      
+      it('should remove unnecessary information from the movies', () => {
+      //set-up
+        const expected = ['title', 'date', 'opening'];
+        //execution
+        let cleanMovies = cleanMovieText(mockuncleanMovies);
+        //expectation
+        expect(Object.keys(cleanMovies[0])).toEqual(expected);
+      });
+      
+    });
+    describe('cleanItemList', () => {
+      let mockUncleanItemList;
+      let mockCleanItemList;
+
+      beforeEach(() => {
+        
+        window.fetch = jest.fn().mockImplementation(() => Promise.resolve({ok: true, json: () => Promise.resolve(mockUncleanItemList)}));
+        
+        mockUncleanItemList = {results: [
+          { name: 'bob', species: 'human', homeworld: 'Earth', popHome: '500', somethingElse: 'info that will later be removed from this object', residents: []},
+
+          { name: 'bill', species: 'droid', homeworld: 'saturn', popHome: '1500', somethingElse: 'info that will later be removed from this object', residents: []}
+        ]};
+        mockCleanItemList = [
+          { name: 'bob', species: 'human', homeworld: 'Earth', popHome: '500'},
+
+          { name: 'bill', species: 'droid', homeworld: 'saturn', popHome: '1500'}
+        ];
+      });
+
+      it('should call the right function depending on the page-- people', async () => {
+        const expected = Object.keys(mockCleanItemList);
+        let cleanItem = await cleanItemList('people', mockUncleanItemList);
+        expect(Object.keys(cleanItem)).toEqual(expected);
+      });
+
+      it('should call the right function depending on the page-- vehicles', async () => {
+        const expected = ['Name', 'Model', 'Class', 'NumPassengers'];
+        let cleanItem = await cleanItemList('vehicles', mockUncleanItemList);
+        expect(Object.keys(cleanItem[0])).toEqual(expected);
+      });
+
+      it('should call the right function depending on the page-- planets', async () => {
+        const expected = ['Name', 'Terrain', 'Population', 'Climate', 'Residents'];
+        let cleanItem = await cleanItemList('planets', mockUncleanItemList);
+        expect(Object.keys(cleanItem[0])).toEqual(expected);
+      });
+
+      it('should return an empty array if it does not recognize the page', async () => {
+      //execution
+        let cleanItem = await cleanItemList('shiggityBop', mockUncleanItemList);
+        expect(cleanItem).toEqual([]);
       //expectation
-      expect(cleanMovies.length).toEqual(expected);
+      });
     });
       
-    it('should remove unnecessary information from the movies', () => {
-      //set-up
-      const expected = ['title', 'date', 'opening'];
-      //execution
-      let cleanMovies = cleanMovieText(mockuncleanMovies);
-      //expectation
-      expect(Object.keys(cleanMovies[0])).toEqual(expected);
-    });
       
   });
-  describe('cleanItemList', () => {
-    let mockUncleanItemList;
-    let mockCleanItemList;
-
-    beforeEach(() => {
-        
-      window.fetch = jest.fn().mockImplementation(() => Promise.resolve({ok: true, json: () => Promise.resolve(mockUncleanItemList)}));
-        
-      mockUncleanItemList = {results: [
-        { name: 'bob', species: 'human', homeworld: 'Earth', popHome: '500', somethingElse: 'info that will later be removed from this object', residents: []},
-
-        { name: 'bill', species: 'droid', homeworld: 'saturn', popHome: '1500', somethingElse: 'info that will later be removed from this object', residents: []}
-      ]};
-      mockCleanItemList = [
-        { name: 'bob', species: 'human', homeworld: 'Earth', popHome: '500'},
-
-        { name: 'bill', species: 'droid', homeworld: 'saturn', popHome: '1500'}
-      ];
-    });
-
-    it('should call the right function depending on the page-- people', async () => {
-      const expected = Object.keys(mockCleanItemList);
-      let cleanItem = await cleanItemList('people', mockUncleanItemList)
-      expect(Object.keys(cleanItem)).toEqual(expected)
-    });
-
-    it('should call the right function depending on the page-- vehicles', async () => {
-      const expected = ['Name', 'Model', 'Class', 'NumPassengers'];
-      let cleanItem = await cleanItemList('vehicles', mockUncleanItemList)
-      expect(Object.keys(cleanItem[0])).toEqual(expected)
-    });
-
-    it('should call the right function depending on the page-- planets', async () => {
-      const expected = ['Name', 'Terrain', 'Population', 'Climate', 'Residents'];
-      let cleanItem = await cleanItemList('planets', mockUncleanItemList)
-      expect(Object.keys(cleanItem[0])).toEqual(expected)
-    });
-
-    it('should pass in the uncleanItemList.results to the cleaner', () => {
-      //set-up
-      
-
-      //execution
-      //expectation
-    });
-
-    it('should pass in the uncleanItemList.results to the cleaer', () => {
-      //set-up
-      //execution
-      //expectation
-    });
-
-    it('should pass in the uncleanItemList.results to the cleaer', () => {
-      //set-up
-      //execution
-      //expectation
-    });
-    //Test that the functions inside have been run a certain number of times.
-    
-  });
-
-  // describe('randomNumber', () => {
-    
-  //   it('should return a randomNumber between 0 and 6', () => {
-  //     //set-up
-  //     const expected = (<= 0 && >= 6);
-  //     //execution
-  //     let rndm = Math.floor(Math.random() * Math.floor(6));
-  //     //expectation
-  //     expect(rndm).toBe(expected);
-  //   });
-      
-  //   });
-      
-  // });
 
 });
