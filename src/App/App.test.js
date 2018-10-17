659ea5cf5f08cd4caaabe5cd9ed5fb1995bc9f6d
Sara Ebbers <johndoe@example.com>
2018-10-17 08:54:51 -0600

    Add local storage test, not yet working

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App', () => {
+  let mockStats;
+  let wrapper;
 
-// 'movies state begins as an empty array, and is set upon calling componentDidMount'
+  beforeEach( ()=>{
+     mockStats = [
+      { name: 'bob', species: 'human', homeworld: 'Earth', popHome: '500'},
+      { name: 'bill', species: 'droid', homeworld: 'saturn', popHome: '1500'}
+      ];
+    wrapper = shallow(<App />)
+  })
 
-// 'movies state is updated to contain an array of objects with cleaned data'
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('should remove an item from favorites if clicked as no longer being a favorite', () => {
+    const expected = [
+      { name: 'bob', species: 'human', homeworld: 'Earth', popHome: '500'}
+      ];
+    localStorage.setItem('favorites', JSON.stringify(mockStats));
+    wrapper.instance().handleFavorites({name: 'bill', species: 'droid', homeworld: 'saturn', popHome: '1500'});
+    console.log(localStorage.getItem('favorites'));
+    // const getItem = localStorage.getItem('favorites');
+    // expect(JSON.parse(getItem)).toEqual(expected)
+
+  }) 
+  // 'movies state begins as an empty array, and is set upon calling componentDidMount'
+
+  // 'movies state is updated to contain an array of objects with cleaned data'
+})
