dd466d4107d816d63480e4cb65dca87790dbf4ec
Aaron Weissman <weissman.aaron@gmail.com>
2018-10-18 15:58:33 -0600

    navigation tests

--- src/navigation/navigation.js
+++ src/navigation/navigation.js
@@ -9,39 +9,38 @@ class Navigation extends Component {
 
   render() {
     const { totalFavorites, currentPage, changePage } = this.props;
-    // const themeSong = new Audio('./theme-song.mp3')
     return (
       <div className='navigation'>
         <ul className='nav-ul'>
-          <NavLink to='/' onClick={() => changePage('home')}>
+          <NavLink className='nav-link-home' to='/' onClick={() => changePage('home')}>
             <li 
               className={currentPage === 'home' ? 'current-nav-btn' : 'nav-btn'}
             >
               Home
             </li>
           </NavLink>
-          <NavLink to='/people' onClick={() => changePage('people')}>
+          <NavLink className='nav-link-people' to='/people' onClick={() => changePage('people')}>
             <li 
               className={currentPage === 'people' ? 'current-nav-btn' : 'nav-btn'}
             >
               People
             </li>
           </NavLink>
-          <NavLink to='/planets' onClick={() => changePage('planets')}>
+          <NavLink className='nav-link-planets' to='/planets' onClick={() => changePage('planets')}>
             <li 
               className={currentPage === 'planets' ? 'current-nav-btn' : 'nav-btn'}
             >
               Planets
             </li>          
           </NavLink>
-          <NavLink to='/vehicles' onClick={() => changePage('vehicles')}>
+          <NavLink className='nav-link-vehicles' to='/vehicles' onClick={() => changePage('vehicles')}>
             <li 
               className={currentPage === 'vehicles' ? 'current-nav-btn' : 'nav-btn'}
             >
               Vehicles
             </li>
           </NavLink>
-          <NavLink to='/favorites' onClick={() => changePage('favorites')}>
+          <NavLink className='nav-link-favorites' to='/favorites' onClick={() => changePage('favorites')}>
             <li 
               className={currentPage === 'favorites' ? 'current-nav-btn' : 'nav-btn'}
             >
