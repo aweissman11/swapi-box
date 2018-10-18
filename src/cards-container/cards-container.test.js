e8cc34f191a35de3d937e1435af56bf302459962
Aaron Weissman <weissman.aaron@gmail.com>
2018-10-18 09:42:26 -0600

    merge in new tests

---
+++ src/cards-container/cards-container.test.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+
+import CardsContainer from './cards-container';
+import * as Helper from '../helper';
+
+describe('CardsContainer', () => {
+
+  let currentPage;
+  let handleFavorites;
+  let wrapper;
+  let mockList;
+   
+  beforeEach(() => {
+    handleFavorites = jest.fn();
+    currentPage = 'people';
+
+    wrapper = shallow(<CardsContainer 
+      getItemList={Helper.getItemList}
+      currentPage={currentPage}
+      handleFavorites={handleFavorites}
+    />);
+
+    mockList = [
+      {
+        Name: 'Luke',
+        Species: 'Human',
+        Homeworld: 'Earth',
+        Population: 7000000000
+      },
+      {
+        Name: 'Jimbo',
+        Species: 'Dinosaur',
+        Homeworld: 'Earth',
+        Population: 56789
+      }
+    ];
+  });
+  
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+  
+  it('should start with empty pageRepo in state', () => {
+    wrapper = mount(<CardsContainer 
+      getItemList={Helper.getItemList}
+      currentPage={currentPage}
+      handleFavorites={handleFavorites}
+    />);
+    expect(wrapper.instance().state.pageRepo).toEqual([]);
+  });
+  
+  it('listCardsshould list all the cards', () => {
+    wrapper = mount(<CardsContainer 
+      getItemList={Helper.getItemList}
+      currentPage={currentPage}
+      handleFavorites={handleFavorites}
+    />);
+    expect(wrapper.instance().state.pageRepo).toEqual([]);
+    wrapper.setState({
+      pageRepo: mockList
+    });
+    expect(wrapper.instance().listCards().length).toEqual(2);
+  });
+}); 
+
